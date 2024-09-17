c6eaf081f0db1b2a45a7447a55cbb8a682e8f60e
Mr. Mueller <bmueller@hstat.org>
2024-09-17 15:31:57 -0400

    add writeup generator script

---
+++ generate.sh
@@ -0,0 +1,12 @@
+# once template is ready, this will duplicate the template to generate the correctly-named writeups
+# use `sh generate.sh` to run (or just copy/paste the code below)
+cp template.md writeups/01-primitives.md
+cp template.md writeups/02-objects.md
+cp template.md writeups/03-boolean-ifs.md
+cp template.md writeups/04-iteration.md
+cp template.md writeups/05-classes.md
+cp template.md writeups/06-arrays.md
+cp template.md writeups/07-arraylist.md
+cp template.md writeups/08-2d-arrays.md
+cp template.md writeups/09-inheritance.md
+cp template.md writeups/10-recursion.md
